1cf11c45be396234882d68040976f190f1bf3bb1
武藤優哉 <metagurosyuya@gmail.com>
2016-12-29 21:32:22 +0900

    ファイル読み込みを自動でやるように変更

--- url_convert.py
+++ url_convert.py
@@ -1,16 +1,16 @@
+def url_convert():
+    f = open('input.txt', 'r')
+    f2 = open('output.txt','w')
+    url_list = []
+    i=0
+    for (i,line) in enumerate(f) :
+        if(i%3==0):
+            item = line.replace("\"","")
+            print(item)
+            print("i")
+            url_list.append(item)
+            f2.write(item)
+        i += 1
 
-f = open('input.txt', 'r')
-f2 = open('output.txt','w')
-url_list = []
-i=0
-for line in f:
-    if(i%3==0):
-        item = line.replace("\"","\'").replace("\n",",\n")
-        print(item)
-        print("i")
-        url_list.append(item)
-        f2.write(item)
-    i += 1
-
-f.close()
-f2.close()
+    f.close()
+    f2.close()
