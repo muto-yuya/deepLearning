dbe4f7160a69308d1c06c9704494440a3af297a8
武藤優哉 <metagurosyuya@gmail.com>
2016-12-29 20:43:42 +0900

    最初のコミット

---
+++ url_convert.py
@@ -0,0 +1,16 @@
+
+f = open('input_text.txt', 'r')
+f2 = open('out_put.txt','w')
+url_list = []
+i=0
+for line in f:
+    if(i%3==0):
+        item = line.replace("\"","\'").replace("\n",",\n")
+        print(item)
+        print("i")
+        url_list.append(item)
+        f2.write(item)
+    i += 1
+
+f.close()
+f2.close()
