994cc63043ba3f1629a0075d6d86e771aa9430cc
武藤優哉 <metagurosyuya@gmail.com>
2017-01-02 17:12:21 +0900

    画像をnparrayに変化する

---
+++ image_to_nparray.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from make_face_extracted import checkFileNum
+person_name = "fujii"
+
+def image_convert(filename):
+    file = cv2.imread(filename)
+    return file
+
+samples_length = checkFileNum(person_name+"_detected_learning")
+image_array=[]
+for i in range(0,samples_length-1):
+    print(samples_length)
+    img = image_convert(person_name+"_detected_learning/"+person_name+"_face"+str(i)+".jpg")
+    print(img)
+    image_array.append(img)
+
+np.save(person_name+"_face.npy",image_array)
